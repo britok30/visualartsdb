6a4ebde9c59f17b0b154046b37eb5091dbd99109
Kelvin Brito <britok30@gmail.com>
2026-04-05 13:25:09 -0400

    abort fix

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,8 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!, {
-  fetchOptions: { signal: AbortSignal.timeout(10000) },
-});
+const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
