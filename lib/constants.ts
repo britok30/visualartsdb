be4b305982213d532dbbb89dd14019ace9b15565
Kelvin Brito <britok30@gmail.com>
2026-05-05 20:15:02 -0400

    db update

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@
 // Update these after each sync to keep SEO descriptions accurate.
 export const SITE_STATS = {
   artworks: "1,000,000+",
-  artists: "100,000+",
+  artists: "85,000+",
   styles: "1,800+",
   genres: "2,300+",
   museums: "385+",
